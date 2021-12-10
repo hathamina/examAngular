94bec155245e1c6988c2b8238e3332b85d396627
Amina <hathamina@gmail.com>
2021-12-10 12:30:31 +0100

    exercice 5 composant

--- evaluationAngular/src/app/app.module.ts
+++ evaluationAngular/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, StatistiqueComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
